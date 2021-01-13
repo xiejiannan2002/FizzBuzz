c93d11655f01ae688981d5a207a1a16e39be28d4
xiejiannan2002 <xiejiannan2002@users.noreply.github.com>
2021-01-13 19:20:44 +1100

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFizzBuzz();
+            Console.ReadLine();
+        }
+
+        public static string GetFizzBuzz(int number)
+        {
+            if (number % 15 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+
+        public static void PrintFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(GetFizzBuzz(i));
+            }
+        }
+    }
+}
---
+++ UnitTestFizzBuzz/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace UnitTestFizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetFizzBuzz()
+        {
+            Assert.AreEqual(Program.GetFizzBuzz(1), "1");
+            Assert.AreEqual(Program.GetFizzBuzz(3), "Fizz");
+            Assert.AreEqual(Program.GetFizzBuzz(5), "Buzz");
+            Assert.AreEqual(Program.GetFizzBuzz(15), "FizzBuzz");
+        }
+    }
+}
